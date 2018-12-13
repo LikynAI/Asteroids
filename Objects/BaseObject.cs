75b1a6fca2dc3a6e57725e5eba13288893a25c84
LikynAI <Likyn2001@gmail.com>
2018-12-13 17:22:33 +0300

    Подправил раздение

---
+++ Objects/BaseObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace game
+{
+	/// <summary>
+	/// Базовый класс объекта
+	/// </summary>
+	abstract class BaseObject : ICollision
+	{
+		public Point Pos;
+		public Point Dir;
+		public Size Size;
+
+		public static event Message MessageDie;
+
+		public delegate void Message();
+
+		public BaseObject(Point Pos, Point Dir, Size Size)
+		{
+			this.Pos = Pos;
+			this.Dir = Dir;
+			this.Size = Size;
+		}
+		
+		/// <summary>
+		/// Отрисовывает положение объекта
+		/// </summary>
+		public abstract void Draw();
+
+		/// <summary>
+		/// Обновляет положение объекта
+		/// </summary>
+		public abstract void Update();
+
+		public bool Collision(ICollision o) => o.Rect.IntersectsWith(this.Rect);
+
+		public Rectangle Rect => new Rectangle(Pos, Size);
+	}
+}
